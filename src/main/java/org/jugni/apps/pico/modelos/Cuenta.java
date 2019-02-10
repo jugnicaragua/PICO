78f68d4da6c991844069a09c6788288b10f9c45f
Nadie, Omar Berroteran <omar.berroteran@javanicaragua.org>
2019-02-10 13:11:45 -0600

    Estableciendo los parametros iniciales de la clase Entidad Cuenta.

--- src/main/java/org/jugni/apps/pico/modelos/Cuenta.java
+++ src/main/java/org/jugni/apps/pico/modelos/Cuenta.java
@@ -1,5 +1,31 @@
 package org.jugni.apps.pico.modelos;
 
-public class Cuenta {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
+/**
+ * Clase que representa la entidad o tabla Cuentas.
+ * 
+ * @author Nadie.
+ *
+ */
+@Entity(name = "Cuentas")
+public class Cuenta {
+  @Id
+  @GeneratedValue
+  Long id; // Usamos una Clase en lugar de una primitiva por mejor practica.
+  private Long idCuenta;
+  private Long IdTipoCuenta;
+  private String codigo;
+  private String descripcion;
+
+  /**
+   * Constructor Vacio requerido
+   */
+  public Cuenta() {
+    
+  }
+  
+  
 }
