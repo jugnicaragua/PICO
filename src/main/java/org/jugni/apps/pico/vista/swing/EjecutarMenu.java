734d1d6570da255cffc1110126c71493fa1817b2
GACS <gacsnic75@gmail.com>
2019-04-13 13:06:06 -0600

    Se agrega la clase EjecutarMenu Esta escucha las acciones que generan los menus

---
+++ src/main/java/org/jugni/apps/pico/vista/swing/EjecutarMenu.java
@@ -0,0 +1,18 @@
+/*
+ * Ejecuta las acciones del menu
+ */
+package org.jugni.apps.pico.vista.swing;
+/**
+ * Clase que ejecuta las intrucciones del menu
+ * @author gacs
+ *
+ */
+public class EjecutarMenu {
+	public EjecutarMenu() {}
+	
+	public void ejecutarAcercaDe() {
+		System.out.println("Bandera");
+		
+	}
+
+}
