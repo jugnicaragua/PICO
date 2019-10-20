75eed1c42806d44824240a991292e1fe114fcb4f
GACS <gacsnic75@gmail.com>
2019-10-19 19:45:13 -0600

    Se agraga clase para prueba de los modelos

---
+++ src/test/java/org/jugni/apps/pico/ModelTest.java
@@ -0,0 +1,42 @@
+package org.jugni.apps.pico;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.jugni.apps.pico.modelos.TipoCuenta;
+import org.jugni.apps.pico.vista.swing.HibernateUtil;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ModelTest {
+    private static TipoCuenta modelo1;
+    private static TipoCuenta modelo2;
+    private List<TipoCuenta> lstDatos;
+    final String SQLQUERY="FROM TipoCuenta";
+    @BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		modelo1 = new TipoCuenta("Activo");
+        modelo2 = new TipoCuenta("Circulante");
+	}
+
+	@Test
+	public void test() {
+	    Session session=HibernateUtil.getSessionFactory().openSession();
+	    Transaction transaction =session.beginTransaction() ;
+        session.save(modelo1);
+        session.save(modelo2);
+        transaction.commit();
+        lstDatos= session.createQuery(SQLQUERY).getResultList();
+        assertNotNull(lstDatos);
+        assertEquals(2, lstDatos.size());
+	}
+
+	@AfterClass
+	public static void setUpAfterClass() throws Exception {
+		HibernateUtil.shutdown();
+	}	
+}
