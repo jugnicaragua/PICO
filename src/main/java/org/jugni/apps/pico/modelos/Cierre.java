61b33870286b718db06b218ae5266f367f2fc19c
GACS <gacsnic75@gmail.com>
2019-11-16 16:27:54 -0600

    Se crea la entidad Cierre

---
+++ src/main/java/org/jugni/apps/pico/modelos/Cierre.java
@@ -0,0 +1,126 @@
+package org.jugni.apps.pico.modelos;
+
+import java.util.Date;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+/**
+ * Clase que representa la entidad o tabla Cierre.
+ * org.jugni.apps.pico.modelos.Cierre
+ *
+ * @author :Gustavo Castro <gacsnic75@gmail.com>
+ * @license : GPLv3
+ */
+@Entity
+public class Cierre {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "Id")
+    private Long id;
+
+    @Column(name = "Anio")
+    private Short anio;
+
+    @Column(name = "Mes")
+    private Byte mes;
+
+    @Column(name = "Fecha")
+    private Date fecha;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "CierreTipoId")
+    private CierreTipo cierreTipo;
+
+    /**
+     * Constructor Vacio requerido, para cuando no se tiene los valores al
+     * instanciar la clase Cierre
+     */
+    public Cierre() {
+    }
+
+    public Cierre(Short anio, Byte mes, Date fecha, CierreTipo cierreTipo) {
+        this.anio = anio;
+        this.mes = mes;
+        this.fecha = fecha;
+        this.cierreTipo = cierreTipo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Short getAnio() {
+        return anio;
+    }
+
+    public void setAnio(Short anio) {
+        this.anio = anio;
+    }
+
+    public Byte getMes() {
+        return mes;
+    }
+
+    public void setMes(Byte mes) {
+        this.mes = mes;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public CierreTipo getCierreTipo() {
+        return cierreTipo;
+    }
+
+    public void setCierreTipo(CierreTipo cierreTipo) {
+        this.cierreTipo = cierreTipo;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cierre other = (Cierre) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Cierre{" + "id=" + id + ", anio=" + anio + ", mes=" + mes + ", fecha=" + fecha + ", cierreTipo=" + cierreTipo + '}';
+    }
+    
+    
+
+}
