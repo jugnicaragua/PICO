4c6f158d8c637eef76370d6a09c84aea311c1189
Juan Selva <jselva@epictechcorp.com>
2020-06-22 14:06:40 -0600

    Error en la forma de publicar mis cambios.
    Revert "Merge branch 'develop' of https://github.com/jugnicaragua/PICO into develop"
    
    This reverts commit 62c7bde22cc2e2b4e00c8c41a2c12a7af4705e3d, reversing
    changes made to d4df67d30337cebd3eab4ff3946643e294126196.

--- src/main/java/org/jugni/apps/pico/modelos/CuentaTipo.java
+++ src/main/java/org/jugni/apps/pico/modelos/CuentaTipo.java
@@ -14,60 +14,63 @@ import javax.persistence.Table;
  *
  */
 @Entity
-@Table(name = "CuentaTipo", indexes = {@Index(name = "idx_tipocuenta", columnList = "Descripcion")})
-public class CuentaTipo extends Identificador<Short> {
+@Table(name="CuentaTipo" ,
+indexes = {@Index(name = "idx_tipocuenta", columnList="Descripcion")})
+public class CuentaTipo extends Identificador<Byte> {
 
-  @Column(name = "Descripcion", length = 50)
-  private String descripcion;
+    @Column(name = "Descripcion", length = 50)
+    private String descripcion;
 
-  public CuentaTipo(String descripcion) {
-    this.descripcion = descripcion;
-  }
-
-
-  /**
-   * Constructor Vacio requerido, para cuando no se tiene los valores al instanciar la clase
-   * TipoCuenta
+//    @OneToMany(mappedBy = "cuentaTipo",
+//        cascade = CascadeType.ALL)
+ //   private List<Cuenta> cuentas= new ArrayList<>() ;
+    
+    public CuentaTipo(String descripcion){
+	this.id = null;
+	this.descripcion = descripcion;
+    }
+    /**
+    * Constructor Vacio requerido, para cuando no se tiene los valores 
+    * al instanciar la clase TipoCuenta
    */
-  public CuentaTipo() {}
+    public CuentaTipo() {
+    }
 
-  /**
-   * Devuelve el nombre del tipo de cuenta
-   * 
-   * @return String
-   */
-  public String getDescripcion() {
-    return descripcion;
-  }
+    /**
+     * Devuelve el nombre del tipo de cuenta
+     * @return String
+     */
+    public String getDescripcion() {
+	return descripcion;
+    }
+    /**
+     * Establece la descripcion del tipo de cuenta
+     * @param descripcion
+     */
+    public void setDescripcion(String descripcion) {
+    	this.descripcion = descripcion;
+    }
+	
+    /**
+     * Nos devuelve las cuentas que se relacionan con el tipo de cuenta
+     * @return List<Cuenta>
+     */
+  /*  public List<Cuenta> getCuentas() {
+	return cuentas;
+    }
+	
+    /**
+     * Establece las cuentas que le corresponde a tipo de cuenta
+     * @param cuentas
+     */
+  /*  public void setCuentas(List<Cuenta> cuentas) {
+    	this.cuentas = cuentas;
+    }
+*/
 
-  /**
-   * Establece la descripcion del tipo de cuenta
-   * 
-   * @param descripcion
-   */
-  public void setDescripcion(String descripcion) {
-    this.descripcion = descripcion;
-  }
-
-  /**
-   * Nos devuelve las cuentas que se relacionan con el tipo de cuenta
-   * 
-   * @return List<Cuenta>
-   */
-  /*
-   * public List<Cuenta> getCuentas() { return cuentas; }
-   * 
-   * /** Establece las cuentas que le corresponde a tipo de cuenta
-   * 
-   * @param cuentas
-   */
-  /*
-   * public void setCuentas(List<Cuenta> cuentas) { this.cuentas = cuentas; }
-   */
-
-  @Override
-  public String toString() {
-    return "CuentaTipo{" + "id=" + id + ", descripcion=" + descripcion + ", cuentas=" + '}';
-  }
+    @Override
+    public String toString() {
+        return "CuentaTipo{" + "id=" + id + ", descripcion=" + descripcion + ", cuentas=" + '}';
+    }
 
 }
