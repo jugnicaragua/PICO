dcbfa846e5ecfa2ab325ed54f3980c98f74aa4a9
Juan Selva <jselva@epictechcorp.com>
2020-06-24 11:29:22 -0600

    Agregar documentación a BaseDao

--- src/main/java/org/jugni/apps/pico/DAO/BaseDao.java
+++ src/main/java/org/jugni/apps/pico/DAO/BaseDao.java
@@ -9,11 +9,24 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
+import com.sun.istack.NotNull;
 
 
 /**
  * Clase base para la creación de clases dao, la clase define las operaciones DML básicas dentro de
  * una base de datos relacional como son SELECT, UPDATE, DELETE.
+ * 
+ * class ClaseDao extends BaseDao<Clase, Integer> {
+ * 
+ * public ClaseDao(SessionFactory factory){ super(factory, Clase.class) }
+ * 
+ * }
+ * 
+ * try (var dao = new ClaseDao()) {
+ * 
+ * //operations dao.save(T entity); ...
+ * 
+ * }
  * 
  * @author jselva
  *
@@ -26,6 +39,12 @@ public class BaseDao<T, ID extends Object> implements Closeable {
   private final Session SESSION;
   private final String TABLE_NAME;
 
+  /**
+   * Constructor por defecto
+   * 
+   * @param factory el session factory utilizado para la obtención de Session
+   * @param entityClass Class del type generico, utilizado para obtener propiedades de la clase.
+   */
   public BaseDao(SessionFactory factory, Class<T> entityClass) {
     Annotation entityAnnotation = entityClass.getAnnotation(Entity.class);
     if (entityAnnotation == null) {
@@ -43,26 +62,54 @@ public class BaseDao<T, ID extends Object> implements Closeable {
     this.SESSION = factory.openSession();
   }
 
+  /**
+   * Devuelve el nombre de la tabla de base de datos.
+   * 
+   * @return Table Name.
+   */
+  @NotNull
   public String getTableName() {
     return this.TABLE_NAME;
   }
 
+  /**
+   * Obtiene la session para la creación de nuevos metodos en las clases hijas.
+   * 
+   * @return Session
+   */
+  @NotNull
   protected Session getSession() {
     return this.SESSION;
   }
 
-  public T get(ID id) {
+  /**
+   * Devuelve un unico elemento de base de datos, utilizando el id de la tabla.
+   * 
+   * @param id
+   * @return Una instancia de <T> o null.
+   */
+  public T get(@NotNull ID id) {
     Query<T> query = this.SESSION.createQuery(" From " + this.TABLE_NAME + " where Id = :id");
     query.setParameter("id", id);
     return query.getSingleResult();
   }
 
+  /**
+   * Obtiene todos los registros de una base de datos.
+   * 
+   * @return una lista de elementos <T> o una lista vacia.
+   */
   public List<T> getAll() {
     Query<T> query = this.SESSION.createQuery(" From " + this.TABLE_NAME);
     return query.getResultList();
   }
 
-  public void save(T entity) {
+  /**
+   * Inserta o actualiza un registro en la base de datos.
+   * 
+   * @param entity La entidad a actualizar o insertar.
+   */
+  public void save(@NotNull T entity) {
     Transaction transaction = this.SESSION.beginTransaction();
     try {
       this.SESSION.saveOrUpdate(entity);
@@ -73,7 +120,12 @@ public class BaseDao<T, ID extends Object> implements Closeable {
     }
   }
 
-  public void saveAll(List<T> entities) {
+  /**
+   * Guarda una lista de elementos en la base de datos.
+   * 
+   * @param entities La lista de elementos a guardar.
+   */
+  public void saveAll(@NotNull List<T> entities) {
     Transaction transaction = this.SESSION.beginTransaction();
     try {
       for (var entity : entities) {
@@ -87,7 +139,12 @@ public class BaseDao<T, ID extends Object> implements Closeable {
     }
   }
 
-  public void remove(T entity) {
+  /**
+   * Elimina un registro de la base de datos.
+   * 
+   * @param entity Entidad a ser eliminada.
+   */
+  public void remove(@NotNull T entity) {
     Transaction transaction = this.SESSION.beginTransaction();
     try {
       this.SESSION.remove(entity);
@@ -98,6 +155,9 @@ public class BaseDao<T, ID extends Object> implements Closeable {
     }
   }
 
+  /**
+   * Elimina todos los registros de la tabla en la base de datos.
+   */
   public void removeAll() {
     Transaction transaction = this.SESSION.beginTransaction();
     try {
